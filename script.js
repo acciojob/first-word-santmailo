8c90035f0cf9a1976daf557d24c6efc64b545fd2
Sunil Sharma <76843848+santmailo@users.noreply.github.com>
2024-12-12 21:22:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,11 +3,11 @@ function firstWord(s) {
 	let str;
   // your code here
 	
-	str = s.split(" ")[0];
+	str = s.trim().split(" ")[0];
 	return str; 
 }
 
 // Do not change the code below 
 
-const s = prompt("Enter String:");
-alert(firstWord(s));
+// const s = prompt("Enter String:");
+// alert(firstWord(s));
